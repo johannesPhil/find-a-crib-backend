e2e7abd4a139b26fd3b94d22b368a05f42545984
johannesPhil <jay3pee@gmail.com>
2020-02-08 03:33:17 +0100

    querydb

--- index.php
+++ index.php
@@ -7,4 +7,8 @@
     else{
         echo "Nope";
     }
+    $q = mysqli_query($conn, "INSERT INTO agents (name, phone, mail, password) VALUES ('john', '45677', 'bhum@njtki.com', 'nfn7f8fjv')");
+    if($q){
+        echo "INSERTED!";
+    }
 ?>
