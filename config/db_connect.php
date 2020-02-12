c3a8c7d91bc0bcaa19110c4931ab5f1b8eb0f7ba
johannesPhil <jay3pee@gmail.com>
2020-02-12 16:56:04 +0100

    change

--- config/db_connect.php
+++ config/db_connect.php
@@ -5,6 +5,10 @@
     $username = $url["user"];
     $password = $url["pass"];
     $db = substr($url["path"], 1);
+    // $server = 'http://findacrib.heliohost.org/';
+    // $username = 'jay3pee';
+    // $password = 'peacerock1';
+    // $db = 'jay3pee_findacrib';
 
     $conn = new mysqli($server, $username, $password, $db);
 ?>
--- index.php
+++ index.php
@@ -3,17 +3,17 @@
     $conn = new mysqli($server, $username, $password, $db);
     
     if($conn){
-        echo "HelioHost MYSQL!!!!!";
+        echo "Hello";
     }
     else{
         echo "Nope";
     }
     
-    $q = "INSERT INTO agents (name, phone, mail, password) VALUES ('John', '1234567', 'mail.com', 'password')";
-    $q_query = mysqli_query($conn, $q);
-    if($q_query){
-        echo "INSERTED!";
-    }
-    echo var_dump($q);
-    mysqli_errno;
+    // $q = "INSERT INTO agents (name, phone, mail, password) VALUES ('John', '1234567', 'mail.com', 'password')";
+    // $q_query = mysqli_query($conn, $q);
+    // if($q_query){
+    //     echo "INSERTED!";
+    // }
+    // echo var_dump($q);
+    // mysqli_errno;
 ?>
